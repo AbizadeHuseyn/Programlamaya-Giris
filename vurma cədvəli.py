0bc930624e6a5ee14001fd254a477848c8971362
AbizadeHuseyn <53041613+AbizadeHuseyn@users.noreply.github.com>
2019-07-18 14:04:21 +0400

    çarpım taplosu
    uzunda olsa çözüm

---
+++ vurma cədvəli.py
@@ -0,0 +1,57 @@
+l1= []
+l2= []
+l3= []
+l4= []
+l5= []             # ÇÖZÜMMÜDÜR? ÇÖZÜMDÜR
+l6= []
+l7= []
+l8= []
+l9= []
+l10= []
+for i in range(1,11):
+    for m in range (1,11):
+        if i==1:
+            k=i*m
+            l1.append(k)
+        if i== 2:
+            k = i *m
+            l2.append(k)
+        if i== 3:
+            k = i *m
+            l3.append(k)
+        if i== 4:
+            k = i *m
+            l4.append(k)
+        if i== 5:
+            k = i *m
+            l5.append(k)
+        if i== 6:
+            k = i *m
+            l6.append(k)
+        if i== 7:
+            k = i *m
+            l7.append(k)
+        if i== 8:
+            k = i *m
+            l8.append(k)
+        if i== 9:
+            k = i *m
+            l9.append(k)
+        if i== 10:
+            k = i *m
+            l10.append(k)
+
+print(l1)
+print(l2)
+print(l3)
+print(l4)
+print(l5)
+print(l6)
+print(l7)
+print(l8)
+print(l9)
+print(l10)
+
+            
+        
+    
